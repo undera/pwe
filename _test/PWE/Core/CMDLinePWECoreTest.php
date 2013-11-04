05771425028abd73f842f5bbd9b5239738210832
Andrey Pohilko <apc@apc.kg>
2013-11-04 19:53:06 +0400

    Fix unit test

--- _test/PWE/Core/CMDLinePWECoreTest.php
+++ _test/PWE/Core/CMDLinePWECoreTest.php
@@ -2,27 +2,18 @@
 
 namespace PWE\Core;
 
-use RuntimeException;
-
-class CMDLinePWECoreTest extends \PHPUnit_Framework_TestCase {
+class CMDLinePWECoreTest extends \PHPUnit_Framework_TestCase
+{
 
     /**
      * @var CMDLinePWECore
      */
     protected $object;
 
-    public function testGetNode() {
+    public function testGetNode()
+    {
         $this->object = new CMDLinePWECore(tempnam('/tmp', 'registry'));
         $this->object->getNode();
-    }
-
-    public function testcreate_fail() {
-        try {
-            $this->object = new CMDLinePWECore('');
-            $this->fail();
-        } catch (RuntimeException $e) {
-            
-        }
     }
 
 }
