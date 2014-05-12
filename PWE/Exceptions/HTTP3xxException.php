7866ed867b95e680d4955f7dd3b9e5298021aa7c
Andrey Pohilko <apc@apc.kg>
2014-05-13 00:39:58 +0400

    Fix bad logging

--- PWE/Exceptions/HTTP3xxException.php
+++ PWE/Exceptions/HTTP3xxException.php
@@ -12,7 +12,7 @@ class HTTP3xxException extends PWEHTTPException
     function __construct($message, $code = HTTP3xxException::REDIRECT)
     {
         parent::__construct($message, $code);
-        PWELogger::info($message, $this);
+        PWELogger::info("Redirect to address %s: %s", $message, $this);
         if ($code == 301 || $code == 302) {
             if (!headers_sent()) {
                 // TODO: use full URI since http 1.1 require it
